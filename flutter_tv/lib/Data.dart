a2cf91a88e4ff6fdcfd4703c759b61329927f4d3
Константин Худышка <k.iroshnikov@itmegastar.com>
2021-02-09 14:22:26 +0700

    парсер листа

--- flutter_tv/lib/Data.dart
+++ flutter_tv/lib/Data.dart
@@ -1,10 +1,26 @@
-
-
+//подробней о декодировании джейсона здесь
+//https://bezkoder.com/dart-flutter-parse-json-string-array-to-object-list/
 class DataTV {
   String name;
-  String symbol;
-  int rank;
-  double price;
+  String username;
+  int id;
+  String email;
 
-  DataTV({this.name, this.symbol, this.rank, this.price});
+  DataTV(this.name, this.username, this.id, this.email);
+
+  factory DataTV.fromJson(dynamic json) {
+    return DataTV(
+      json['name'] as String, 
+      json['username'] as String,
+      json['id'] as int,
+      json['email'] as String
+      );
+  }
+
+  //функция получения джейсона из модели
+  //
+  // @override
+  // String toString() {
+  //   return '{ ${this.name}, ${this.username}, ${this.id}, ${this.email} }';
+  // }
 }
--- flutter_tv/lib/FluterTV.dart
+++ flutter_tv/lib/FluterTV.dart
@@ -2,13 +2,21 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_tv/Data.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class ListTV extends StatelessWidget {
-  List<DataTV> data = [
-    DataTV(name: 'Bitcoin', symbol: 'BTC', rank: 1, price: 6000.0),
-    DataTV(name: 'Etherium', symbol: 'ETH', rank: 2, price: 200.0),
-    DataTV(name: 'Trololo', symbol: 'BTC', rank: 3, price: 500.0),
-  ];
+
+class ListTV extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ListTVState();
+  }
+}
+
+
+class ListTVState extends State<ListTV> {
+  
+  List<DataTV> data = [];
 
 
   @override
@@ -22,17 +30,47 @@ class ListTV extends StatelessWidget {
           children: _buildList(),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: () => _loadCC(),
+      ),
     );
   }
 
   List<Widget> _buildList() {
     return data.map((DataTV f) => ListTile(
-      subtitle: Text(f.symbol),
+      subtitle: Text(f.username),
       title: Text(f.name),
-      leading: CircleAvatar(child: Text(f.rank.toString()),),
-      trailing: Text('\$${f.price.toString()}'),
+      leading: CircleAvatar(child: Text(f.id.toString()),),
+      trailing: Text(f.email),
     )).toList();
   }
 
-  
+  void _loadCC() async {
+    //await значит выполняется на другом потоке
+    final response = await http.get('https://jsonplaceholder.typicode.com/users');
+    if (response.statusCode == 200){
+       var tagObjsJson = jsonDecode(response.body) as List;
+       List<DataTV> datasObjs = tagObjsJson.map((tagJson) => DataTV.fromJson(tagJson)).toList();
+
+      print(datasObjs);
+      print(datasObjs.first.email);
+
+      // var allData = json.decode(response.body) as List<<String, dynamic>>;
+      // List<DataTV> dataList = [];
+
+      // allData.forEach((key, value) { 
+      //   var record = DataTV(
+      //     name: value['name'], 
+      //     username: value['username'],
+      //     id: value['id'],
+      //     email: value['email']
+      //   );
+      //   dataList.add(record);
+      // });
+
+      // print(dataList);
+    }
+  }
+
 }
